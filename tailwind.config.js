58d5308625b09428e8b38f15eec529305ed911aa
mpietryka <mat.pietryka@gmail.com>
2022-10-22 18:24:32 +0100

    Tailwind, DaisyUI and Firebase Initialised

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  daisyui: {
+    themes: ["business"],
+  },
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
