5b49896820ad16954df70263ed77f812e1dd232f
mpietryka <mat.pietryka@gmail.com>
2022-10-24 17:41:27 +0100

    dummy content for privacy policy and terms and condition pages

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Hero = () => {
   return (
@@ -6,13 +7,24 @@ export const Hero = () => {
       <div className="hero bg-base-100 ">
         <div className="hero-content text-center">
           <div className="max-w-md">
-            <h1 className="text-5xl font-bold">Hello there</h1>
+            <h1 className="text-5xl font-bold">Hello there!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
               et a id nisi.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <p className="pb-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">
+            <Link to="/pricing">
+                <span className="mx-4 opacity-80 transition-opacity hover:opacity-100">
+                  Get Started
+                </span>
+              </Link>
+            </button>
           </div>
         </div>
       </div>
--- src/pages/termsandconditions/TermsandConditions.js
+++ src/pages/termsandconditions/TermsandConditions.js
@@ -5,9 +5,41 @@ export const TermsandConditions = () => {
   return (
     <div>
       <Navbar />
-      <p className="text-center text-3xl mt-10">
-        Terms and Conditions Coming Soon!
-      </p>
+      <h1 className="text-5xl font-bold mt-12 text-center">Terms and Conditions</h1>
+      <div className="text-center w-1/2 mx-auto m-12">
+        <p className="my-4">
+          Enim veniam sint irure non. Anim occaecat adipisicing nostrud nisi
+          laboris cillum aute. Aliquip occaecat amet nisi fugiat.
+        </p>
+        <p className="my-4">
+          Fugiat aliqua do commodo tempor mollit laboris eu nulla laborum elit
+          sit ipsum. Sit eu sit deserunt est tempor aliqua consequat officia.
+          Proident dolore consectetur ea irure eu tempor. Culpa dolore do
+          nostrud dolore reprehenderit voluptate proident nostrud. Qui duis eu
+          proident dolor nisi dolor ipsum fugiat. Ea occaecat cupidatat do
+          cupidatat nostrud id ex eiusmod aliquip consequat culpa incididunt
+          nisi ex.
+        </p>
+        <p className="my-4">
+          Eiusmod et Lorem reprehenderit nulla duis aliqua dolor Lorem aute et
+          aliqua magna. Sint excepteur anim exercitation mollit. Culpa nisi et
+          et nostrud proident do fugiat velit ea labore ut aliqua labore ex.
+          Lorem qui deserunt aute ex Lorem sunt esse excepteur esse ut. Est quis
+          id duis minim voluptate. Commodo irure proident eu sunt ipsum.
+        </p>
+        <p className="my-4">
+          Eu officia cillum do non aliquip deserunt velit ex exercitation nisi.
+          Fugiat voluptate consequat non proident duis sit consequat ad et.
+          Pariatur officia do amet eiusmod do officia est officia deserunt
+          dolore quis adipisicing sint dolor. Do aliqua do consequat est
+          excepteur do fugiat tempor ipsum consequat ad do Lorem laboris. Dolor
+          sint enim aute incididunt cupidatat esse id quis occaecat. Veniam
+          irure pariatur reprehenderit adipisicing consectetur laborum
+          excepteur. Excepteur exercitation laboris pariatur ullamco eiusmod
+          esse fugiat aute adipisicing qui nisi.
+        </p>
+        <p className="my-4">Dolor culpa duis magna veniam culpa sint sunt id fugiat cillum occaecat.</p>
+      </div>
       <Footer />
     </div>
   );
